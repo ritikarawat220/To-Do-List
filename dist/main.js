f48083163e652568c06fd0b2752b068923b95ff4
Ritika Rawat <Ritikarawat220@gmail.com>
2023-02-21 20:10:16 +0530

    Add to-do list webpack

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.body.appendChild(function(){alert("Helloo....");const e=document.createElement("div");return e.innerHTML=_.join('["Hello","webpack"]',""),e}())})();
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
