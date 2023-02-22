ba1cfbc3851bb40483b876b67a6fbdb7e9c428ae
Ritika Rawat <Ritikarawat220@gmail.com>
2023-02-22 22:30:13 +0530

    Add & remove done for to do list

--- src/index.js
+++ src/index.js
@@ -1,34 +1,50 @@
 import "./index.css";
 
-const tasks = [
-    {
-      index: 1,
-      description: "structure todo list",
-      completed: false,
-    },
-    {
-      index: 2,
-      description: "add task todo list",
-      completed: false,
-    },
-    {
-      index: 3,
-      description: "remove todo list",
-      completed: false,
-    },
-  ];
+/* import toDoTask from "./modules/todo.js";
 
-  function populateList() {
-    const todoList = document.getElementById("todo-list");
-  
-    tasks.forEach((item) => {
-      const listItem = document.createElement("li");
-      listItem.innerHTML = `
-      <input class="checkbox" type="checkbox" name="" value="">
-      <span>${item.description}</span>
-      `;
-      todoList.appendChild(listItem);
-    });
+class toDoTask {
+  constructor(desc, index, completed = false) {
+    this.desc = desc;
+    this.index = index;
+    this.completed = completed;
   }
-  
-  populateList();
+} */
+
+//Add a task
+document.querySelector("#push").onclick = function(){
+  if (document.querySelector("#newtask input").
+  value.length == 0){
+    alert("Please Enter a Task!...");
+  }else {
+    document.querySelector("#tasks").innerHTML
+    += `
+    <div class="task">
+    <span id="taskname">
+    ${document.querySelector("#newtask input").value}
+    </span>
+    <button class="delete">
+    <i class="fa-sharp fa-solid fa-xmark"></i>
+    </button>
+    </div>
+    `;
+
+    //delete a task
+    var current = document.querySelectorAll(".delete");
+    for(let i = 0; i < current.length; i++){
+      current[i].onclick = function(){
+        this.parentNode.remove();
+      };
+    }
+
+    var tasks = document.querySelectorAll(".task");
+    for(let i = 0; i <tasks.length; i++){
+      tasks[i].onclick = function(){
+        this.classList.toggle("completed");
+      };
+    }
+
+    document.querySelector("#newtask input").value = "";
+  }
+};
+
+
